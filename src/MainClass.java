a0dfc60ed86797596f7e88ca484a7f8cb78d3001
FilipM <Filipmarc71@gmail.com>
2019-09-26 17:23:59 +0200

    Third learning commit.

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,6 @@
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Uczę\nsię\nprogramowania\ni\ncommitowania");
+        System.out.println("Kolejna zmiana w kodzie");
     }
 }
